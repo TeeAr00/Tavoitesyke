15ee3b2bafcef357886e2f0be2784764f3891fbb
TeeAr00 <t1rate00@student.oamk.fi>
2022-09-30 10:18:50 +0300

    syke laskettu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [Upper,setUpper] = useState(0)
+  const [Lower,setLower] = useState(0)
+  const [Age,setAge] = useState(0)
+
+  function laske(e){
+    e.preventDefault()
+    const ala = (220-Age) * 0.65
+    const yla = (220-Age) * 0.85
+    setLower (ala)
+    setUpper(yla)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="content">
+      <h3>Heart rate limits calculator</h3>
+      <form onSubmit={laske}>
+        <div>
+        <label>Age</label>
+        <input value={Age} onChange={e => setAge(e.target.value)}/>
+        </div>
+        <div>
+          <label>Heart rate limits</label>
+          <output>{Lower.toFixed(0) +"-" + Upper.toFixed(0)}</output>
+        </div>
+        <button>Calculate</button>
+      </form>
     </div>
+    
   );
 }
 
